642de99d03210a5032a18993de40777f8a3614b7
Jvitaliy82 <jvit82@gmail.com>
2020-07-04 23:15:13 +0700

    add options menu

--- app/src/main/java/com/jdeveloperapps/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/jdeveloperapps/telegram/ui/fragments/SettingsFragment.kt
@@ -1,9 +1,19 @@
 package com.jdeveloperapps.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.jdeveloperapps.telegram.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 
 }
