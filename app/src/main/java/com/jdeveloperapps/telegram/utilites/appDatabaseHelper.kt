18dd1d02be1c00ff4485c180ff2c4232aaa25779
Jvitaliy82 <jvit82@gmail.com>
2020-07-07 19:48:34 +0700

    add ReyclerView in contacts

--- app/src/main/java/com/jdeveloperapps/telegram/utilites/appDatabaseHelper.kt
+++ app/src/main/java/com/jdeveloperapps/telegram/utilites/appDatabaseHelper.kt
@@ -3,6 +3,7 @@ package com.jdeveloperapps.telegram.utilites
 import android.net.Uri
 import android.provider.ContactsContract
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -113,3 +114,4 @@ fun upfatePhoneToDatabase(arrayContacts: ArrayList<CommonModel>) {
     })
 }
 
+fun DataSnapshot.getCommonModel(): CommonModel = this.getValue(CommonModel::class.java)?: CommonModel()
